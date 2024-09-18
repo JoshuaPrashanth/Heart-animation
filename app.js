bee1376fdee96e41d90aa2254019dfbc1d77e17b
Joshua Prashanth <codewithjosh12@gmail.com>
2024-09-19 00:05:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const body = document.querySelector("body");
+
+body.addEventListener("toucnmove", (event)=>{
+    event.preventDefault();
+    const xpos = event.offsetX;
+    const ypos = event.offsetY;
+    const poo = document.createElement("span");
+    body.append(poo);
+    poo.style.left = xpos + "px";
+    poo.style.top = ypos + "px";
+    const size = Math.random()*100;
+    poo.style.height = size + "px";
+    poo.style.width = size + "px";
+    setTimeout(() => {
+        poo.remove();
+    }, 2000);
+})
