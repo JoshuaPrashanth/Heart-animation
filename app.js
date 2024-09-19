f6a78af6103bf055e6fc81f5b6b5f8932d4f0618
Joshua Prashanth <codewithjosh12@gmail.com>
2024-09-19 09:29:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const body = document.querySelector("body");
 
-body.addEventListener("touchmove", (event)=>{
-    
+body.addEventListener("click", (event)=>{
+    event.preventDefault();
     const xpos = event.offsetX;
     const ypos = event.offsetY;
     const poo = document.createElement("span");
     body.append(poo);
     poo.style.left = xpos + "px";
-    poo.style.bottom = ypos + "px";
+    poo.style.top = ypos + "px";
     const size = Math.random()*100;
     poo.style.height = size + "px";
     poo.style.width = size + "px";
